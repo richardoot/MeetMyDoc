2c0361d28052da4176d5fa7f52662a2194268dca
Richard Boilley <richardoot11@gmail.com>
2019-03-17 17:55:09 +0100

    Utilisation du AppFixture pour ajouter un utilisateur patient en base de données

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,27 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //Création d'un générateur de données faker
+          //$faker = Faker\Factory::create('fr_FR');
 
+          $user = new User();
+
+          $user->setEmail("richard@user.com");
+          $user->setRoles(['ROLE_PATIENT']);
+          $user->setPassword('$2y$10$qaHa4SF6o1ECaZoc6.xCluHRnlImOPwReLffIjagZhQzM8s59Lk7i');
+          $user->setNom("Richard");
+          $user->setPrenom("Boilley");
+          //$date = new DateTime();
+          $user->setDateNaissance(new \dateTime());
+          $user->setSexe("Masculin");
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
