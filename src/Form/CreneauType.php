965c63a25ef5216211732fe09cb4691cfe58603a
kesly gassant <kellorkeskes@hotmail.fr>
2019-03-23 18:52:03 +0100

    creation du formulaire pour l'entité Creneau

---
+++ src/Form/CreneauType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Creneau;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreneauType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('etat')
+            ->add('horaireDebut')
+            ->add('horaireFin')
+            ->add('duree')
+            ->add('medecin')
+            ->add('patient')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Creneau::class,
+        ]);
+    }
+}
