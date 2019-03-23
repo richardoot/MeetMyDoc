da82250b518ad2409fa0a30dd20740dd2b4cf0fc
kesly gassant <kellorkeskes@hotmail.fr>
2019-03-23 18:10:58 +0100

    creation de l'entité Creneau

---
+++ src/Entity/Creneau.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CreneauRepository")
+ */
+class Creneau
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $etat;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $horaireDebut;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $horaireFin;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $duree;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getHoraireDebut(): ?\DateTimeInterface
+    {
+        return $this->horaireDebut;
+    }
+
+    public function setHoraireDebut(\DateTimeInterface $horaireDebut): self
+    {
+        $this->horaireDebut = $horaireDebut;
+
+        return $this;
+    }
+
+    public function getHoraireFin(): ?\DateTimeInterface
+    {
+        return $this->horaireFin;
+    }
+
+    public function setHoraireFin(\DateTimeInterface $horaireFin): self
+    {
+        $this->horaireFin = $horaireFin;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(int $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+}
